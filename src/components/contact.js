3d56e48fdc8658369055efe1e531941c6f1bf837
kyh012412 <kyh0124.12@gmail.com>
2024-03-17 16:35:56 +0900

    14 end about deploy

--- src/components/contact.js
+++ src/components/contact.js
@@ -51,9 +51,9 @@ export default function AppContact() {
         <iframe
           title="map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.6986440037745!2d126.72070332715674!3d37.49143722842883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b7c45cf5a2007%3A0x8e67185bbcae5145!2z67aA7Y-J7Jet!5e0!3m2!1sko!2skr!4v1710653825913!5m2!1sko!2skr"
-          allowfullscreen=""
+          allowFullScreen=""
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
       <Container fluid>
